6ec906e1252255c923d87587da47713c5f3c6721
Gappyjosuke <sastharuban24@gmail.com>
2025-06-15 00:17:00 +0530

    interpreter: evaluate literal, variable, unary, and binary expressions

--- src/interpreter.c
+++ src/interpreter.c
@@ -40,34 +40,62 @@ int get_variable(const char* name) {
     fprintf(stderr, "Runtime Error: Undefined variable '%s'\n", name);
     exit(1);
 }
-
 int eval_expr(ASTNode* expr) {
+    if (!expr) {
+        fprintf(stderr, "eval_expr called with NULL!\n");
+        exit(1);
+    }
+    
+    if (expr == NULL) {
+     fprintf(stderr, "Runtime Error: NULL expression node\n");
+     exit(1);
+    }
+
+    if (expr->type < 0 || expr->type > AST_VARIABLE) {
+        fprintf(stderr, "[SANITY] Invalid expr->type: %d (%s)\n", expr->type, ast_type_to_str(expr->type));
+    }
+
     switch (expr->type) {
         case AST_LITERAL:
             return expr->literal.value;
-        case AST_VARIABLE: 
+
+        case AST_VARIABLE:
             return get_variable(expr->variable.name);
-        
+
+        case AST_UNARY_OP: {
+            int val = eval_expr(expr->unary_op.operand);
+            return (expr->unary_op.op == TOKEN_MINUS) ? -val : val;
+        }
+
         case AST_BINARY_OP: {
             int left = eval_expr(expr->binary_op.left);
             int right = eval_expr(expr->binary_op.right);
-            
+
             switch (expr->binary_op.op) {
-               case TOKEN_PLUS: return left + right;
-               case TOKEN_MINUS: return left - right;
-               case TOKEN_STAR: return left * right;
-               case TOKEN_SLASH: return right != 0 ? left / right : (fprintf(stderr, "Division by zero\n"), exit(1), 0);
-               default:
-                fprintf(stderr, "Unsupported operator: %s\n", token_type_to_string(expr->binary_op.op));
-                exit(1);
+                case TOKEN_PLUS: return left + right;
+                case TOKEN_MINUS: return left - right;
+                case TOKEN_STAR: return left * right;
+                case TOKEN_SLASH:
+                    if (right == 0) {
+                        fprintf(stderr, "Division by zero\n");
+                        exit(1);
+                    }
+                    return left / right;
+                default:
+                    fprintf(stderr, "Unsupported binary operator: %d\n", expr->binary_op.op);
+                    exit(1);
             }
         }
+
         default:
-        fprintf(stderr, "Unknown expression type:%d\n",expr->type);
-        exit(1);
-        
+            fprintf(stderr, "Unknown expression type: %d (%s) (maybe uninitialized or parser bug)\n",
+                expr->type, ast_type_to_str(expr->type));
+            fprintf(stderr, "[DEBUG] Raw node pointer: %p\n", (void*)expr);
+            exit(1);
     }
 }
+
+
 
 void interpret(ASTNode* node) {
     while (node) {
