35db2b55ad62fe8f84f3037cc6a098d1c6f2a962
Gappyjosuke <sastharuban24@gmail.com>
2025-06-14 13:31:09 +0530

    main: connect parser and AST, print AST

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "../include/lexer.h"
 #include "../include/parser.h"
+#include "../include/ast.h"
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -23,12 +24,20 @@ int main(int argc, char* argv[]) {
     fread(input, 1, length, file);
     input[length] = '\0';
     fclose(file);
-
+    
+    //Lexical Analysis
     TokenNode* tokens = tokenize(input);
-    print_tokens(tokens);  // Optional: comment this if not needed
+    print_tokens(tokens);
 
-    parse_program(tokens); // Stubbed in parser.c
+    //Parsing -> AST
+    ASTNode* ast = parse_program(tokens);
 
+    //Print AST (just debug-print)
+    printf("\nAST:\n"):
+    print_ast(ast);
+
+    //cleanup
+    free_ast(ast);
     free_tokens(tokens);
     free(input);
 
